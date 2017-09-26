40745e840289a5af7d81922586c49e170f30f8f5
Mohandor <Emilio.Cadario@gmail.Com>
2017-09-26 14:59:01 +0200

    Ajout de fonctions permettant que le starMin de l'outil ne soit pas supérieur au max et vis versa

--- js/starSelect.js
+++ js/starSelect.js
@@ -2,8 +2,26 @@ var starSelect = {
 	init: function(){
 		$('<div/>').addClass('row').attr('id', 'starSelectRow').appendTo($('#starSelection'));
 		$('<div/>').addClass('col s12 center-align').text('Ne montrer que les restaurants compris dans cette fourchette').appendTo($('#starSelectRow'));
-		$('<div/>').addClass('col s6').attr('id', 'starMin').starRating({initialRating: 0, starSize: 20, disableAfterRate: false}).appendTo($('#starSelectRow'));
-		$('<div/>').addClass('col s6').attr('id', 'starMax').starRating({initialRating: 5, starSize: 20, disableAfterRate: false}).appendTo($('#starSelectRow'));
+		$('<div/>').addClass('col s6').attr('id', 'starMin').starRating({
+			initialRating: 0,
+			starSize: 20, 
+			disableAfterRate: false,
+			callback: function(currentRating, $el){
+				if(currentRating > Number($('#starMax').starRating('getRating'))){
+					$('#starMax').starRating('setRating', currentRating);
+				}
+			}
+		}).appendTo($('#starSelectRow'));
+		$('<div/>').addClass('col s6').attr('id', 'starMax').starRating({
+			initialRating: 5,
+			starSize: 20,
+			disableAfterRate: false,
+			callback: function(currentRating, $el){
+				if(currentRating < Number($('#starMin').starRating('getRating'))){
+					$('#starMin').starRating('setRating', currentRating);
+				}
+			}
+		}).appendTo($('#starSelectRow'));
 		$('<a/>').addClass('waves-effect waves-light btn center').attr('id', "btnStarSelect").text('Classer').appendTo($('#starSelectRow'));
 		$('<i/>').addClass('material-icons right').text('restaurant').appendTo($('#btnStarSelect'));
 
